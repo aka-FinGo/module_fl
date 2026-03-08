e89661b38b51cf453d2d2752467aca29d2c4a148
aka-FinGo <akafingo@gmail.com>
2026-03-08 15:14:31 +0500

    Create api_repository.dart

---
+++ lib/data/repositories/api_repository.dart
@@ -0,0 +1,61 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/module_model.dart';
+
+// API Manzili (O'zingizning Google Sheets Deploy URL manzilingizni shu yerga qo'ying)
+const String apiUrl = 'https://script.google.com/macros/s/AKfycbxyWSaRdn-4NZkkwJiAb2Q-uezsE8U_iFhjdSfsd4vZRHodaQ-aLhMNZe9ZwqjdVMjQ/exec';
+
+// Global holat provayderlari
+final scannedBarcodeProvider = StateProvider<String?>((ref) => null);
+final moduleDataProvider = StateProvider<ModuleModel?>((ref) => null);
+final isLoadingProvider = StateProvider<bool>((ref) => false);
+
+final apiRepositoryProvider = Provider<ApiRepository>((ref) {
+  return ApiRepository(Dio());
+});
+
+class ApiRepository {
+  final Dio _dio;
+
+  ApiRepository(this._dio) {
+    _dio.options.connectTimeout = const Duration(seconds: 15);
+    _dio.options.receiveTimeout = const Duration(seconds: 15);
+  }
+
+  Future<ModuleModel> fetchModuleData(String barcode) async {
+    try {
+      final response = await _dio.get(
+        apiUrl,
+        queryParameters: {'barcode': barcode},
+      );
+
+      if (response.statusCode == 200) {
+        // String kelsa, uni JSON (Map) ga o'tkazish
+        final responseData = response.data is String 
+            ? response.data 
+            : response.data;
+            
+        return ModuleModel.fromJson(responseData);
+      } else {
+        return ModuleModel(
+          artikul: '', nomi: '', pdfUrl: '', videoUrl: '', furnituralar: {}, 
+          error: 'Server xatosi: ${response.statusCode}'
+        );
+      }
+    } on DioException catch (e) {
+      String errorMessage = 'Tarmoq xatosi yoki CORS muammosi!';
+      if (e.type == DioExceptionType.connectionTimeout) {
+        errorMessage = 'Ulanish vaqti tugadi. Internetni tekshiring.';
+      }
+      return ModuleModel(
+        artikul: '', nomi: '', pdfUrl: '', videoUrl: '', furnituralar: {}, 
+        error: errorMessage
+      );
+    } catch (e) {
+      return ModuleModel(
+        artikul: '', nomi: '', pdfUrl: '', videoUrl: '', furnituralar: {}, 
+        error: 'Noma\'lum xato: $e'
+      );
+    }
+  }
+}
