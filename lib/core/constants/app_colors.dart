526ca1a4f1ceacb72e65b93afe6e50383f8daa7d
aka-FinGo <akafingo@gmail.com>
2026-03-08 15:04:25 +0500

    Create app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF2C3E50);
+  static const Color accent = Color(0xFF27AE60);
+  static const Color background = Color(0xFFF5F6FA);
+  static const Color textDark = Color(0xFF333333);
+  static const Color textGray = Color(0xFF7F8C8D);
+  static const Color danger = Color(0xFFE74C3C);
+  static const Color warning = Color(0xFFF1C40F);
+  static const Color bottomNavBg = Colors.white;
+}
