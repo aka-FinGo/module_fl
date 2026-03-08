e195c75217684bd15825423bcfeada61106ab4be
aka-FinGo <akafingo@gmail.com>
2026-03-08 15:05:11 +0500

    Create home_page.dart

---
+++ lib/features/home/presentation/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Tarix ro\'yxati bu yerda bo\'ladi', style: TextStyle(fontSize: 16)),
+    );
+  }
+}
