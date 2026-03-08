08f476ec82f2933ea86e90912b72771018a0746e
aka-FinGo <akafingo@gmail.com>
2026-03-08 15:06:08 +0500

    Create module_details_page.dart

---
+++ lib/features/home/presentation/module_details_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ModuleDetailsPage extends StatelessWidget {
+  const ModuleDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Furnituralar Accordioni bu yerda bo\'ladi', style: TextStyle(fontSize: 16)),
+    );
+  }
+}
