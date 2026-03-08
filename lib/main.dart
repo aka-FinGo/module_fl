101293bc1e6c080d6ba78ecdbead8b28234c4276
aka-FinGo <akafingo@gmail.com>
2026-03-08 15:04:47 +0500

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'core/constants/app_colors.dart';
+import 'features/shell/presentation/shell_page.dart';
+
+void main() {
+  runApp(const ProviderScope(child: AristokratApp()));
+}
+
+class AristokratApp extends StatelessWidget {
+  const AristokratApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aristokrat Mebel',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        scaffoldBackgroundColor: AppColors.background,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.primary,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+          elevation: 0,
+        ),
+        bottomAppBarTheme: const BottomAppBarTheme(
+          color: AppColors.bottomNavBg,
+          elevation: 10,
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          primary: AppColors.primary,
+          secondary: AppColors.accent,
+        ),
+        useMaterial3: true,
+      ),
+      home: const ShellPage(),
+    );
+  }
+}
