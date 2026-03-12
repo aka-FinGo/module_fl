700e00a6660599da8d965e2baaf562e76103c30a
aka-FinGo <aka-FinGo@users.noreply.github.com>
2026-03-12 10:54:25 +0000

    deploy: 4b45ba4cceda47a4a0d7009cb6004e8ddc15e863

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4034405094" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2855417943" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
