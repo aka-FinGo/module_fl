f436fe403329a3e1255177b4cbc4e89d27756cef
aka-FinGo <aka-FinGo@users.noreply.github.com>
2026-04-10 07:10:57 +0000

    deploy: ea7ccfe9cd82c5055d923c1731e401168d880dbb

--- main.dart.js
+++ main.dart.js
@@ -25922,7 +25922,7 @@ $iJr:1}
 A.aoR.prototype={
 $1(a){var s=A.dk().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/e4b8dca3f1b4ede4c30371002441c88c12187ed6/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb/":s)+a},
 $S:74}
 A.JJ.prototype={
 gv(a){var s=this.a
